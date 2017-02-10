5566759bb00ff602576e2880ada99bf43aaeeb18
cbstodd <cbstodd@gmail.com>
2017-02-09 20:52:00 -0500

    added form validation examples

--- src/app/model-form/model-form.component.ts
+++ src/app/model-form/model-form.component.ts
@@ -1,39 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'cc-model-form',
     templateUrl: './model-form.component.html',
     styleUrls: ['./model-form.component.css']
 })
-export class ModelFormComponent implements OnInit {
+export class ModelFormComponent {
 
     signupForm:FormGroup;
+    firstName:FormControl;
+    lastName:FormControl;
+    email:FormControl;
+    password:FormControl;
+    language:FormControl;
 
     langs:string[] = [
-        '-- Select a language (None) --',
-        'English',
-        'French',
-        'German',
-        'Spanish'
+        "English",
+        "French",
+        "German",
+        "Spanish"
     ];
 
     constructor() {
     }
 
-    ngOnInit() {
-        this.signupForm = new FormGroup({
-            name: new FormGroup({
-                firstName: new FormControl(),
-                lastName: new FormControl()
-            }),
-            email: new FormControl(),
-            password: new FormControl(),
-            language: new FormControl()
+    createFormControls() {
+        this.firstName = new FormControl('', Validators.required);
+        this.lastName  = new FormControl('', Validators.required);
 
-        })
+        this.email    = new FormControl('', [
+            Validators.required,
+            Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$')
+        ]);
+        this.password = new FormControl('', [
+            Validators.required,
+            Validators.minLength(8)
+        ]);
+        this.language = new FormControl('');
     }
 
+
+    createForm() {
+        this.signupForm = new FormGroup({
+            name: new FormGroup({
+                firstName: this.firstName,
+                lastName: this.lastName,
+            }),
+            email: this.email,
+            password: this.password,
+            language: this.language
+        });
+
+    }
+
+    ngOnInit() {
+        this.createFormControls();
+        this.createForm();
+    }
 
 
 }
